5f70f97adb3f522486b50c06e9aac5385c76f7a3
Christian Nuss <christian.nuss@gmail.com>
2021-11-22 14:07:30 -0800

    add a basic cli

---
+++ cli/index.ts
@@ -0,0 +1,30 @@
+import { NsLookup } from '../src';
+
+function usage() {
+  // eslint-disable-next-line no-console
+  console.error('Usage: soans [domain]');
+  process.exit(-1);
+}
+
+(async () => {
+  try {
+    const domain = process.argv.slice(2)[0];
+    if (!domain) {
+      usage();
+      return;
+    }
+    const nameservers = await NsLookup(domain);
+    nameservers.addresses.forEach((address) => {
+      // eslint-disable-next-line no-console
+      console.log(address);
+    });
+  } catch (e) {
+    if (e instanceof Error) {
+      // eslint-disable-next-line no-console
+      console.error(`Lookup error: `, e.message);
+      process.exit(-1);
+    }
+    throw e;
+  }
+  process.exit(0);
+})();
